46fd9a98eb8fff4f7cc6bfccbb96cd1482fffc0b
Monirul-RS <monir030599@gmail.com>
2022-10-11 23:25:25 +0600

    404 committt

--- src/components/ErrorPage/ErrorPage.js
+++ src/components/ErrorPage/ErrorPage.js
@@ -4,8 +4,10 @@ import { Link } from 'react-router-dom';
 const ErrorPage = () => {
     return (
         <div>
-            <h1 className='text-4xl font-bold text-blue-600 mt-14'>404 <span className='text-3xl text-blue-500' > This page cannot be found!</span></h1>
-            <h1 className='text-2xl'>Go to the Home page <Link className='underline' to='/'>Topics</Link></h1>
+            <div className='mt-32'>
+                <h1 className='text-4xl font-bold text-blue-600 '>404 <span className='text-3xl text-blue-500' > This page cannot be found!</span></h1>
+                <h1 className='text-3xl mt-4'>Go to the Home page <Link to='/'><button className='btn btn-info'>Topics</button></Link></h1>
+            </div>
         </div>
     );
 };
