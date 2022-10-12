9eb7999ee1ed9984166b69474ece1f74a751ea48
Monirul-RS <monir030599@gmail.com>
2022-10-12 15:22:14 +0600

    toast commit

--- src/components/Quiz/Quiz.js
+++ src/components/Quiz/Quiz.js
@@ -1,19 +1,47 @@
 import React from 'react';
+import { ToastContainer, toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
+
 
 const Quiz = ({ question }) => {
+    const { options, correctAnswer } = question;
+
+    // const correctAnswer=() =>{
+    //     alert('Right Answer:' +correctAnswer)
+    // }
+    const checkOption = test => {
+        if (test === correctAnswer) {
+            toast('Yeah! Correct Answer',
+            {
+                    position: "top-center",
+                    autoClose: 5000,
+                    hideProgressBar: false,
+                    closeOnClick: true,
+                    pauseOnHover: true,
+                    draggable: true,
+                    progress: undefined,
+                    theme: "light",
+                });
+        }
+        else{
+            toast.error('Oops! Wrong Answer')
+        }
+    }
     return (
         <div className='custom-quiz'>
-            {question.options.map((option) => (
+            {options.map((option) => (
                 <div className='flex border border-sky-400 p-4 rounded-lg bg-slate-100'>
-                    
-                        <div>
-                            <input type="radio" value={option} className='mr-2 mt-4' /> {option}
-                        </div>
-                    
+
+                    <div>
+                        <input onClick={() => checkOption(option)} type="radio" value={option} name='quiz' className='mr-2 mt-4' /> {option};
+                        {/* // <button onClick={() => checkOption(option)}>{option}</button> */}
+                    </div>
+
                     {/* <div>{option}</div> */}
 
                 </div>
             ))}
+            <ToastContainer position="top-center" />
 
         </div>
     );
