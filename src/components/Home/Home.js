53bb49e3deb79cee73c912c2545695bad227fb5f
Monirul-RS <monir030599@gmail.com>
2022-10-12 12:58:33 +0600

    commit

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -2,6 +2,9 @@ import React from 'react';
 import { useLoaderData } from 'react-router-dom';
 import Topic from '../Topic/Topic';
 import img from '../images/Banner3.jpg'
+
+
+
 const Home = () => {
     const topics = useLoaderData().data;
     // console.log(topics);
@@ -9,13 +12,14 @@ const Home = () => {
         <div>
             <div>
                 <div className='lg:flex border-2 p-4'>
-                    <img className='lg:w-full h-96 border md:w-full rounded-xl' src={img} alt="" /> 
+                    <img className='lg:w-full h-96 border md:w-full rounded-xl' src={img} alt="" />
                 </div>
                 <div className='border-0 m-6 p-8'>
-                   <h1 className=' text-indigo-600 text-6xl  font-bold ml-3 '>Welcome to Tera Quiz
-                    <br />
-                    <span className='text-indigo-400 text-4xl'>Test your skills and practice.</span>
-                    </h1> </div> 
+                    <h1 className=' text-indigo-600 text-6xl  font-bold ml-3 '>Welcome to Tera Quiz
+                        <br />
+                        <span className='text-indigo-400 text-4xl'>Test your skills and practice.</span>
+                    </h1>
+                </div>
             </div>
             <div className='grid lg:grid-cols-4 md:grid-cols-2 p-9'>
                 {
